65389b969680a7e57ed54b86c3c9b04f729a507f
Jaesuk <jaesuk.hwang@gmail.com>
2016-03-05 16:17:03 +0900

    Deploying to Heroku still needs more works.

--- web/project.clj
+++ web/project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/Lion-Hwang/sound-effector"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/java.jdbc "0.4.2"]
                  [org.postgresql/postgresql "9.4.1208"]
@@ -14,13 +15,10 @@
                  [commons-validator/commons-validator "1.5.0"]]
   :main ^:skip-aot sound-effector.web
   :uberjar-name "sound-effector-standalone.jar"
-  :plugins [[lein-ring "0.9.7"]
-            [lein-bower "0.5.1"]]
+  :plugins [[lein-ring "0.9.7"]]
   :ring {:handler sound-effector.web/application
          :init    sound-effector.models.schema/migrate}
   :profiles {:dev     {:dependencies [[javax.servlet/servlet-api "2.5"]
                                       [ring-mock "0.1.5"]]}
-             :uberjar {:aot :all}}
-  :bower {:directory    "resources/public/lib"
-          :dependencies [[bootstrap "3.3.6"]
-                         [font-awesome "4.5.0"]]})
+             :uberjar {:omit-source true
+                       :aot         :all}})
