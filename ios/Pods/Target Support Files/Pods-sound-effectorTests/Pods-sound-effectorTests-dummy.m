840608b48f3e6faaf1fdf03033cf35cd2591dfd0
Jaesuk <jaesuk.hwang@gmail.com>
2016-03-08 01:27:50 +0900

    Added CocoaPods for Alamofire.

---
+++ ios/Pods/Target Support Files/Pods-sound-effectorTests/Pods-sound-effectorTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_sound_effectorTests : NSObject
+@end
+@implementation PodsDummy_Pods_sound_effectorTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-sound-effectorUITests/Pods-sound-effectorUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_sound_effectorUITests : NSObject
+@end
+@implementation PodsDummy_Pods_sound_effectorUITests
+@end
