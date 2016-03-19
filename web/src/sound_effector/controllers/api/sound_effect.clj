8ed1ea1f004b9e47fbf0cdf3121b7a21d620a4f8
Jaesuk <jaesuk.hwang@gmail.com>
2016-03-20 01:03:36 +0900

    Added the query parameter 'q' to API.

--- web/src/sound_effector/controllers/api/sound_effect.clj
+++ web/src/sound_effector/controllers/api/sound_effect.clj
@@ -1,11 +1,14 @@
 (ns sound-effector.controllers.api.sound-effect
   (:require
+    [clojure.string :as string]
     [ring.util.response :refer [response]]
     [compojure.core :refer [defroutes GET]]
     [sound-effector.models.sound-effect :as model]))
 
-(defn show-list []
-  (response (model/read)))
+(defn show-list [query]
+  (if (string/blank? query)
+    (response (model/read))
+    (response (model/search query))))
 
 (defroutes routes
-           (GET "/api/sound-effects" [] (show-list)))
+           (GET "/api/sound-effects*" {params :query-params} (show-list (get params "q"))))
