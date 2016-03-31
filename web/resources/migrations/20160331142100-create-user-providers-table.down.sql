ba59dc5e3ebab0fddfd8cadfa82f594f8ccd8542
Jaesuk <jaesuk.hwang@gmail.com>
2016-03-31 14:47:02 +0900

    Added user_providers table.

---
+++ web/resources/migrations/20160331142100-create-user-providers-table.down.sql
@@ -0,0 +1,5 @@
+DROP INDEX IF EXISTS user_providers_idx1;
+--;;
+DROP INDEX IF EXISTS user_providers_uk1;
+--;;
+DROP TABLE IF EXISTS user_providers;
---
+++ web/resources/migrations/20160331142100-create-user-providers-table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE user_providers (
+  id               SERIAL PRIMARY KEY,
+  user_id          INTEGER   NOT NULL,
+  provider_id      INTEGER   NOT NULL,
+  provider_user_id VARCHAR   NOT NULL,
+  connected_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+--;;
+-- For now, I will not allow that the user connects multiple account for a provider.
+CREATE UNIQUE INDEX user_providers_uk1 ON user_providers (user_id, provider_id);
+--;;
+CREATE INDEX user_providers_idx1 ON user_providers (provider_id, provider_user_id);
