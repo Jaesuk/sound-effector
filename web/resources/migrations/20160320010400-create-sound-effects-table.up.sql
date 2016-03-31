cac72196648330909c30741177033a8567d8acef
Jaesuk <jaesuk.hwang@gmail.com>
2016-03-31 14:35:29 +0900

    Reformatted.

--- web/resources/migrations/20160320010400-create-sound-effects-table.up.sql
+++ web/resources/migrations/20160320010400-create-sound-effects-table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE sound_effects (
-  id SERIAL PRIMARY KEY,
-  title VARCHAR NOT NULL,
-  url VARCHAR NOT NULL,
+  id         SERIAL PRIMARY KEY,
+  title      VARCHAR   NOT NULL,
+  url        VARCHAR   NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
