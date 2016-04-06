cbe9e25479055b7252d4daaa16ea0a8032284ef1
Jaesuk <jaesuk.hwang@gmail.com>
2016-04-07 01:57:48 +0900

    Applied Liberator(http://clojure-liberator.github.io/liberator/). Remove '/api/ context and merged it into '/sound-effects' context.

--- web/src/sound_effector/controllers/sound_effect.clj
+++ web/src/sound_effector/controllers/sound_effect.clj
@@ -1,9 +1,14 @@
 (ns sound-effector.controllers.sound-effect
   (:require
     [clojure.string :as string]
-    [ring.util.response :as ring-response]
-    [compojure.core :refer [defroutes GET POST DELETE]]
+    [clojure.data.json :as json]
+    [ring.util.response :refer [redirect]]
+    [ring.middleware.format :refer [wrap-restful-format]]
+    [ring.middleware.format-response :refer [wrap-restful-response]]
+    [ring.middleware.params :refer [params-request]]
+    [compojure.core :refer [defroutes context GET POST DELETE]]
     [compojure.coercions :refer [as-int]]
+    [liberator.core :refer [resource]]
     [sound-effector.models.sound-effect :as model]
     [sound-effector.views.sound-effect :as view])
   (:import
@@ -12,23 +17,40 @@
 
 (def url-validator (UrlValidator. (into-array ["http" "https"])))
 
-(defn show-list [query]
-  (if (string/blank? query)
-    (view/index (model/read))
-    (view/index (model/search query) query)))
+(defn show-list [request]
+  (let [query (get-in request [:params :q])]
+    (if (string/blank? query)
+      (view/index (model/read))
+      (view/index (model/search query) query))))
+
+(defn show-json-list [request]
+  (let [query (get (:query-params (params-request request)) "q")]
+    (if (string/blank? query)
+      (json/write-str (model/read) :value-fn (fn [_ value] (if (instance? java.sql.Timestamp value) (str value) value)))
+      (model/search query))))
 
 (defn create [title url]
   (if (and (not (string/blank? title)) (.isValid url-validator url))
     (model/create title url))
   ; TODO: error message and parameters will be returned.
-  (ring-response/redirect "/sound-effects"))
+  (redirect "/sound-effects"))
 
 (defn delete [id]
   (model/delete id)
-  (ring-response/redirect "/sound-effects"))
+  (redirect "/sound-effects"))
 
 (defroutes routes
-           (GET "/" [] (ring-response/redirect "/sound-effects"))
-           (GET "/sound-effects" {{query :q} :params} (show-list query))
-           (POST "/sound-effects" [title url] (create title url))
-           (DELETE "/sound-effects/:id" [id :<< as-int] (delete id)))
+           (GET "/" [] (redirect "/sound-effects"))
+           (context "/sound-effects" []
+             (GET "/" []
+               (resource :available-media-types ["text/html" "application/json"]
+                         :handle-ok
+                         #(let [ctx %
+                                media-type (get-in ctx [:representation :media-type])]
+                           (condp = media-type
+                             "text/html" (show-list (:request ctx))
+                             "application/json" (show-json-list (:request ctx))
+                             ))
+                         ))
+             (POST "/" [title url] (create title url))
+             (DELETE "/:id" [id :<< as-int] (delete id))))
--- web/src/sound_effector/web.clj
+++ web/src/sound_effector/web.clj
@@ -3,26 +3,20 @@
     [migratus.core :as migratus]
     [ring.adapter.jetty :as ring]
     [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-    [ring.middleware.format :refer [wrap-restful-format]]
-    [compojure.core :refer [defroutes routes GET]]
-    [compojure.route :as route]
+    [compojure.core :refer [defroutes]]
+    [compojure.route :refer [resources not-found]]
     [sound-effector.controllers.sound-effect :as controller]
-    [sound-effector.controllers.api.sound-effect :as api-controller]
     [sound-effector.core :refer [migratus-config elasticsearch-migrate]]
     [sound-effector.views.layout :as layout])
   (:gen-class))
 
 (defroutes site-routes
            controller/routes
-           (route/resources "/")
-           (route/not-found (layout/response-404)))
+           (resources "/")
+           (not-found (layout/response-404)))
 
-(defroutes api-routes
-           api-controller/routes)
-
-(defroutes application
-           (wrap-restful-format api-routes :formats [:json])
-           (wrap-defaults site-routes site-defaults))
+(def application
+  (wrap-defaults site-routes site-defaults))
 
 (defn start [port]
   (ring/run-jetty application {:port  port
