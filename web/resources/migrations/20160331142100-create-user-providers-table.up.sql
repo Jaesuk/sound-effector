a06d53d224f7463c09c5bddd93bb6dceb7f61b5e
Jaesuk <jaesuk.hwang@gmail.com>
2016-04-11 11:23:57 +0900

    Added 'user_id' column to 'user_providers_idx1' index.

--- web/resources/migrations/20160331142100-create-user-providers-table.up.sql
+++ web/resources/migrations/20160331142100-create-user-providers-table.up.sql
@@ -9,4 +9,4 @@ CREATE TABLE user_providers (
 -- For now, I will not allow that the user connects multiple account for a provider.
 CREATE UNIQUE INDEX user_providers_uk1 ON user_providers (user_id, provider_id);
 --;;
-CREATE INDEX user_providers_idx1 ON user_providers (provider_id, provider_user_id);
+CREATE INDEX user_providers_idx1 ON user_providers (provider_id, provider_user_id, user_id);
