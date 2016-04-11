58874fc493964393428448961fd053bf4e5a2e49
Jaesuk <jaesuk.hwang@gmail.com>
2016-04-11 11:31:39 +0900

    Added Friend and fixed ambiguities.

--- web/project.clj
+++ web/project.clj
@@ -7,20 +7,29 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/java.jdbc "0.4.2"]
                  [org.clojure/data.json "0.2.6"]
+                 [org.clojure/core.cache "0.6.5"]
                  [org.postgresql/postgresql "9.4.1208"]
                  [migratus "0.8.13"]
                  [ring/ring-jetty-adapter "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
+                 [ring-logger "0.7.6"]
                  [compojure "1.4.0"]
                  [liberator "0.13"]
+                 [com.cemerick/friend "0.2.1"]
                  [hiccup "1.0.5"]
+                 [commons-codec/commons-codec "1.10"]
                  [commons-validator/commons-validator "1.5.0"]
+                 [org.apache.httpcomponents/httpclient "4.5.1"]
+                 [clj-http "2.1.0" :exclusions [org.apache.httpcomponents/httpcore]]
+                 [slingshot "0.12.2"]
+                 [cheshire "5.5.0"]
                  [clojurewerkz/elastisch "2.2.1"]]
   :main ^:skip-aot sound-effector.web
   :uberjar-name "sound-effector-standalone.jar"
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler sound-effector.web/application
-         :init    sound-effector.web/migrate}
+         :init    sound-effector.web/migrate
+         :port    3000}
   :profiles {:dev     {:dependencies [[javax.servlet/servlet-api "2.5"]
                                       [ring-mock "0.1.5"]]}
              :uberjar {:omit-source true
