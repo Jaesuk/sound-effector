8cc032ca689a0d00e8991618bdeef91603219359
Jaesuk <jaesuk.hwang@gmail.com>
2016-04-13 00:02:17 +0900

    Added providers map.

--- web/src/sound_effector/models/user.clj
+++ web/src/sound_effector/models/user.clj
@@ -2,6 +2,15 @@
   (:require [clojure.java.jdbc :as sql]
             [sound-effector.core :refer [db-spec]]))
 
+(defn- make-providers [id]
+  (loop [providers (sql/query db-spec ["SELECT provider_id, provider_user_id FROM user_providers WHERE user_id = ?" id])
+         result {}]
+    (if (nil? providers)
+      result
+      (let [provider (first providers)]
+        (recur (next providers)
+               (assoc result (:provider_id provider) (:provider_user_id provider)))))))
+
 (defn create [name email provider-id provider-user-id]
   (sql/with-db-transaction
     [trans-conn db-spec]
@@ -12,10 +21,12 @@
                    :user_providers {:user_id          (:id user)
                                     :provider_id      provider-id
                                     :provider_user_id provider-user-id})
-      user)))
+      (assoc user :providers (make-providers (:id user))))))
 
 (defn read
   ([id]
-   (first (sql/query db-spec ["SELECT * FROM users WHERE id = ?" id])))
+   (let [user (first (sql/query db-spec ["SELECT * FROM users WHERE id = ?" id]))]
+     (assoc user :providers (make-providers (:id user)))))
   ([provider-id provider-user-id]
-   (first (sql/query db-spec ["SELECT a.* FROM users a JOIN user_providers b ON (b.provider_id = ? AND b.provider_user_id = ? AND b.user_id = a.id)" provider-id provider-user-id]))))
+   (let [user (first (sql/query db-spec ["SELECT a.* FROM users a JOIN user_providers b ON (b.provider_id = ? AND b.provider_user_id = ? AND b.user_id = a.id)" provider-id provider-user-id]))]
+     (assoc user :providers (make-providers (:id user))))))
