27be2da81f95e9e4ea1d9411592a9a4adec6ce3c
Jaesuk <jaesuk.hwang@gmail.com>
2016-04-13 15:46:40 +0900

    Applied request to 404 page, too.

--- web/src/sound_effector/views/index.clj
+++ web/src/sound_effector/views/index.clj
@@ -2,7 +2,8 @@
   (:require [sound-effector.views.layout :as layout]))
 
 (defn index [request]
-  (layout/common request
-                 "Sound Effector"
-                 [:h1 "Sound Effector"]
-                 [:div "I don't know what to say! :D"]))
+  (layout/common
+    request
+    "Sound Effector"
+    [:h1 "Sound Effector"]
+    [:div "I don't know what to say! :D"]))
